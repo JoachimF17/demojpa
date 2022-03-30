ba7529633827eea20798a20c914d12c98aeafe3f
Joachim <joachim.frances@hotmail.be>
2022-03-30 09:44:38 +0200

    Initial commit

---
+++ src/test/java/be/technifutur/demojpa/DemojpaApplicationTests.java
@@ -0,0 +1,13 @@
+package be.technifutur.demojpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemojpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
